abdef82ede16acdb08b7c25f2190ed72983753e9
azu <azuciao@gmail.com>
2023-07-08 10:44:34 +0900

    100

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-// 100 items in the array
+// 1000 items in the array
 const items = Array.from({ length: 100 }, (_, i) => i);
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const doHeavyTask = () => {
-    return wait(1000);
-}
 console.log("!!Start");
 // foreach do task and defer 100ms
 items.forEach(async (item) => {
     console.log("Start task: ", item);
-    await doHeavyTask();
-    console.log("End task: ", item);
     await wait(100);
+    console.log("End task: ", item);
 });
 console.log("!!End");
