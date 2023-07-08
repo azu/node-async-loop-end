53689cc4c763fab82f440ca16eda568a17372f4d
azu <azuciao@gmail.com>
2023-07-08 10:44:11 +0900

    CI

--- test.js
+++ test.js
@@ -2,11 +2,15 @@
 const items = Array.from({ length: 100 }, (_, i) => i);
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const doHeavyTask = () => {
+    return wait(1000);
+}
 console.log("!!Start");
 // foreach do task and defer 100ms
 items.forEach(async (item) => {
     console.log("Start task: ", item);
-    await wait(100);
+    await doHeavyTask();
     console.log("End task: ", item);
+    await wait(100);
 });
 console.log("!!End");
